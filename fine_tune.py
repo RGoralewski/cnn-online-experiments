4223e43a95baa69a3931c116e25c206ae1e96dac
RGoralewski <radekgoralewski@gmail.com>
2021-01-12 18:11:46 +0100

    Add 3-fold cross validation, calculate mean test accuracy, adjust params

--- fine_tune.py
+++ fine_tune.py
@@ -12,7 +12,7 @@ from put_emg_gestures_classification.pegc.models import Resnet1D
 from put_emg_gestures_classification.pegc import constants
 from put_emg_gestures_classification.pegc.training import _epoch_train, _validate
 from put_emg_gestures_classification.pegc.training.utils import load_json, \
-    initialize_random_seeds, save_json
+    initialize_random_seeds, save_json, EarlyStopping, EarlyStoppingSignal
 from put_emg_gestures_classification.pegc.training.lookahead import Lookahead
 from put_emg_gestures_classification.pegc.generators import PUTEEGGesturesDataset
 from put_emg_gestures_classification.pegc.training.clr import CyclicLR
@@ -23,7 +23,7 @@ from emg.emglimbo import EMG_Preprocessor
 import matplotlib.pyplot as plt
 
 
-def load_model(model_path: str, model: nn.Module) -> tuple:
+def load_model(model_path: str, model: nn.Module) -> nn.Module:
     # Load model and optimizer from checkpoint
     checkpoint = torch.load(model_path)
     model.load_state_dict(checkpoint['model_state_dict'])
@@ -36,14 +36,35 @@ def unison_shuffled_copies(a, b):
     return a[p], b[p]
 
 
-def fine_tune(model_path: str, data: dict, config_file_path: str, val_split_size: float = 0.15,
-              test_split_size: float = 0.1) -> None:
+def fine_tune(model_path: str, data: dict, config_file_path: str) -> None:
 
     # Read config from file
     fine_tuning_config = load_json(config_file_path)
     device = torch.device('cuda') if torch.cuda.is_available() and not fine_tuning_config["force_cpu"] \
         else torch.device('cpu')
     initialize_random_seeds(constants.RANDOM_SEED)
+
+    # Create specified model.
+    model = Resnet1D(constants.DATASET_FEATURES_SHAPE[0], constants.NB_DATASET_CLASSES,
+                     fine_tuning_config["nb_res_blocks"], fine_tuning_config["res_block_per_expansion"],
+                     fine_tuning_config["base_feature_maps"])
+
+    # Load model
+    model = load_model(model_path, model)
+
+    # Freeze all layers except two first (convolutional and batch normalization) and the last (fully connected)
+    print("Layers to fine tune:")
+    for name, param in model.named_parameters():
+        if ('res_blocks.0.conv_1' in name) or ('dense' in name):
+            param.requires_grad = True
+        else:
+            param.requires_grad = False
+        if param.requires_grad:
+            print(name)
+
+    # Optimizer setup.
+    base_opt = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), fine_tuning_config["base_lr"])
+    optimizer = Lookahead(base_opt, k=5, alpha=0.5) if fine_tuning_config["use_lookahead"] else base_opt
 
     # Load data
     features = np.array(data["features"])
@@ -60,105 +81,119 @@ def fine_tune(model_path: str, data: dict, config_file_path: str, val_split_size
     # Shuffle windows
     features, labels = unison_shuffled_copies(features, labels)
 
-    # Divide data to train, test and validate splits
-    val_size = int(val_split_size * features.shape[0])
-    test_size = int(test_split_size * features.shape[0])
+    # Prepare 3 even splits
+    first_split_end = int(features.shape[0] / 3)
+    second_split_end = int(features.shape[0] * 2 / 3)
 
-    X_train = features[val_size:-test_size]
-    y_train = labels[val_size:-test_size]
+    split_1 = {'X': features[:first_split_end], 'y': labels[:first_split_end]}
+    split_2 = {'X': features[first_split_end:second_split_end], 'y': labels[first_split_end:second_split_end]}
+    split_3 = {'X': features[second_split_end:], 'y': labels[second_split_end:]}
 
-    X_val = features[:val_size]
-    y_val = labels[:val_size]
+    # Cross validation iterations
+    cross_val_it = [{'train': split_1, 'val': split_2, 'test': split_3},
+                    {'train': split_2, 'val': split_3, 'test': split_1},
+                    {'train': split_3, 'val': split_1, 'test': split_2}]
 
-    X_test = features[-test_size:]
-    y_test = labels[-test_size:]
-
-    # Load data to generators
-    train_dataset = PUTEEGGesturesDataset(X_train, y_train)
-    val_dataset = PUTEEGGesturesDataset(X_val, y_val)
-    test_dataset = PUTEEGGesturesDataset(X_test, y_test)
-    train_gen = DataLoader(train_dataset,
-                           batch_size=fine_tuning_config["batch_size"],
-                           shuffle=fine_tuning_config["shuffle"])
-    val_gen = DataLoader(val_dataset,
-                         batch_size=fine_tuning_config["batch_size"],
-                         shuffle=fine_tuning_config["shuffle"])
-    test_gen = DataLoader(test_dataset,
-                          batch_size=fine_tuning_config["batch_size"],
-                          shuffle=fine_tuning_config["shuffle"])
-    # Note: this data is quite simple, no additional workers will be required for loading/processing.
-
-    # Create specified model.
-    model = Resnet1D(constants.DATASET_FEATURES_SHAPE[0], constants.NB_DATASET_CLASSES,
-                     fine_tuning_config["nb_res_blocks"], fine_tuning_config["res_block_per_expansion"],
-                     fine_tuning_config["base_feature_maps"])
-
-    # Load model
-    model = load_model(model_path, model)
-
-    # Freeze all layers except two first (convolutional and batch normalization) and the last (fully connected)
-    for name, param in model.named_parameters():
-        if ('res_blocks.0.' in name) or ('dense' in name):
-            param.requires_grad = True
-        else:
-            param.requires_grad = False
-
-    # Optimizer setup.
-    base_opt = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), fine_tuning_config["base_lr"])
-    optimizer = Lookahead(base_opt, k=5, alpha=0.5) if fine_tuning_config["use_lookahead"] else base_opt
-
-    # Loss function
-    class_shares = np.sum(y_train, axis=0) / len(y_train)
-    class_weights = 1 / class_shares
-    class_weights = class_weights / np.sum(class_weights)  # normalize to 0-1 range
-    loss_fnc = torch.nn.MultiLabelSoftMarginLoss(reduction='mean',
-                                                 weight=torch.tensor(class_weights, dtype=torch.float32).to(device))
-
-    # LR schedulers setup.
-    epochs_per_half_clr_cycle = 4
-    clr = CyclicLR(optimizer, base_lr=fine_tuning_config["base_lr"], max_lr=fine_tuning_config["max_lr"],
-                   step_size_up=len(train_gen) * epochs_per_half_clr_cycle,
-                   mode='triangular2', cycle_momentum=False)
-    schedulers = [clr]
-
-    # Temporarily - for saving stats
-    metrics = []
-    results_dir_path = f"stats_{int(time.time())}"
-    os.makedirs(results_dir_path, exist_ok=True)
-
-    # Fine tuning
+    # 3-fold cross validation - learning three times
+    test_accs = {'before': [], 'after': []}
     start = time.time()
-    for ep in range(1, fine_tuning_config["epochs"] + 1):
-        epoch_stats = _epoch_train(model, train_gen, device, optimizer, loss_fnc, ep, fine_tuning_config["use_mixup"],
-                                   fine_tuning_config["alpha"], schedulers)
-        val_stats = _validate(model, loss_fnc, val_gen, device)
-        epoch_stats.update(val_stats)
+    for it in cross_val_it:
 
-        print(f'\nEpoch {ep} train loss: {epoch_stats["loss"]:.4f}, '
-              f'val loss: {epoch_stats["val_loss"]:.5f}, val_acc: {epoch_stats["val_acc"]:.4f}')
+        # Load data to generators
+        train_dataset = PUTEEGGesturesDataset(it['train']['X'], it['train']['y'])
+        val_dataset = PUTEEGGesturesDataset(it['val']['X'], it['val']['y'])
+        test_dataset = PUTEEGGesturesDataset(it['test']['X'], it['test']['y'])
+        train_gen = DataLoader(train_dataset,
+                               batch_size=fine_tuning_config["batch_size"],
+                               shuffle=fine_tuning_config["shuffle"])
+        val_gen = DataLoader(val_dataset,
+                             batch_size=fine_tuning_config["batch_size"],
+                             shuffle=fine_tuning_config["shuffle"])
+        test_gen = DataLoader(test_dataset,
+                              batch_size=fine_tuning_config["batch_size"],
+                              shuffle=fine_tuning_config["shuffle"])
+        # Note: this data is quite simple, no additional workers will be required for loading/processing.
 
-        metrics.append(epoch_stats)
+        # Loss function
+        class_shares = np.sum(it['train']['y'], axis=0) / len(it['train']['y'])
+        class_weights = 1 / class_shares
+        class_weights = class_weights / np.sum(class_weights)  # normalize to 0-1 range
+        loss_fnc = torch.nn.MultiLabelSoftMarginLoss(reduction='mean',
+                                                     weight=torch.tensor(class_weights, dtype=torch.float32).to(device))
 
-    # Check results on final test/holdout set:
-    test_set_stats = _validate(model, loss_fnc, test_gen, device)
-    print(f'\nFinal evaluation on test set: '
-          f'test loss: {test_set_stats["val_loss"]:.5f}, test_acc: {test_set_stats["val_acc"]:.4f}')
+        # LR schedulers setup.
+        epochs_per_half_clr_cycle = 4
+        clr = CyclicLR(optimizer, base_lr=fine_tuning_config["base_lr"], max_lr=fine_tuning_config["max_lr"],
+                       step_size_up=len(train_gen) * epochs_per_half_clr_cycle,
+                       mode='triangular2', cycle_momentum=False)
+        schedulers = [clr]
 
-    # Save metrics/last network/optimizer state
-    save_json(osp.join(results_dir_path, 'test_set_stats.json'), test_set_stats)
-    save_json(osp.join(results_dir_path, 'training_losses_and_metrics.json'), {'epochs_stats': metrics})
+        callbacks = []
+        if fine_tuning_config["use_early_stopping"]:
+            callbacks.append(EarlyStopping(monitor='val_loss', mode='min',
+                                           patience=fine_tuning_config["early_stopping_patience"]))
+            # Important: early stopping must be last on the list!
+
+        # Temporarily - for saving stats
+        metrics = []
+        results_dir_path = f"stats_{int(time.time())}"
+        os.makedirs(results_dir_path, exist_ok=True)
+
+        # Check results on test/holdout set:
+        test_set_stats = _validate(model, loss_fnc, test_gen, device)
+        print(f'\nEvaluation on test set: '
+              f'test loss: {test_set_stats["val_loss"]:.5f}, test_acc: {test_set_stats["val_acc"]:.4f}')
+        test_accs['before'].append(test_set_stats["val_acc"])
+
+        # Fine tuning
+        try:
+            for ep in range(1, fine_tuning_config["epochs"] + 1):
+                epoch_stats = _epoch_train(model, train_gen, device, optimizer, loss_fnc, ep, fine_tuning_config["use_mixup"],
+                                           fine_tuning_config["alpha"], schedulers)
+                val_stats = _validate(model, loss_fnc, val_gen, device)
+                epoch_stats.update(val_stats)
+
+                print(f'\nEpoch {ep} train loss: {epoch_stats["loss"]:.4f}, '
+                      f'val loss: {epoch_stats["val_loss"]:.5f}, val_acc: {epoch_stats["val_acc"]:.4f}')
+
+                metrics.append(epoch_stats)
+                for cb in callbacks:
+                    cb.on_epoch_end(ep, epoch_stats)
+        except EarlyStoppingSignal as e:
+            print(e)
+
+        # Check results on final test/holdout set:
+        test_set_stats = _validate(model, loss_fnc, test_gen, device)
+        print(f'\nFinal evaluation on test set: '
+              f'test loss: {test_set_stats["val_loss"]:.5f}, test_acc: {test_set_stats["val_acc"]:.4f}')
+
+        # Append test stats to the lists
+        test_accs['after'].append(test_set_stats["val_acc"])
+
+        # Save metrics/last network/optimizer state
+        save_json(osp.join(results_dir_path, 'test_set_stats.json'), test_set_stats)
+        save_json(osp.join(results_dir_path, 'training_losses_and_metrics.json'), {'epochs_stats': metrics})
 
     stop = time.time()
 
+    # Calculate mean accuracy in the end
+    mean_acc_b4 = np.mean(test_accs['before'])
+    std_acc_b4 = np.std(test_accs['before'])
+    mean_acc_aft = np.mean(test_accs['after'])
+    std_acc_aft = np.std(test_accs['after'])
+
     # print time and loss
-    print('****************** '
-          f'Fine tuning time: {stop - start} '
+    print('******************\n'
+          f'Fine tuning time: {stop - start}\n'
+          f'Mean acc before: {mean_acc_b4}, Acc std before: {std_acc_b4}\n'
+          f'Mean acc after: {mean_acc_aft}, Acc std after: {std_acc_aft}\n'
           '******************')
 
     # Save model
+    '''
     torch.save({'model_state_dict': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict()}, model_path)
-
+    '''
 
 def on_message(client, userdata, message):
     """Callback function
@@ -219,7 +254,7 @@ def on_message(client, userdata, message):
                 training_data = features_2d.T
 
                 # One hot label
-                label = np.array([int(gestures_classes[current_gesture_idx] == x) for x in gestures_classes])
+                label = np.array([int(gestures_classes[current_gesture_idx] == x) for x in gestures_classes][:8])
 
                 # Append to the buffer
                 buffer['features'].append(training_data)
@@ -261,7 +296,7 @@ last_sampling_rate_print_time = script_start_time
 # List of gestures
 gestures_classes = ['idle', 'fist', 'flexion', 'extension', 'pinch_index', 'pinch_middle',
                     'pinch_ring', 'pinch_small']
-gest_duration = 3.5
+gest_duration = 5
 time_for_gest_change = 1
 
 # Current gesture
